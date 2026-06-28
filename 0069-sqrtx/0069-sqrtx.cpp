ef5f4ff58098308e07e0de0e81c0292ba0e7045b
Nandani Kandoi <nandanikandoi@gmail.com>
2026-06-28 20:54:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.83%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x == 0) return 0;   // special case
-
-        int lo = 1, hi = x;
+        int lo = 0, hi = x;
         int ans = 0;
         while(lo <= hi) {
-            long long mid = lo + (hi - lo) / 2;   // mid as long long
-            long long sq = mid * mid;             // safe multiplication
+            long long mid = lo + (hi - lo) / 2;
+            long long sq = mid * mid;
             if(sq <= x) {
                 ans = mid;
                 lo = mid + 1;
