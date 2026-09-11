d70c5a91eca16787e17193f8f9ca07680e5b0e7b
Nandani Kandoi <nandanikandoi@gmail.com>
2026-09-11 20:20:10 +0530

    Time: 0 ms (100%), Space: 13.5 MB (15.77%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -12,23 +12,37 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         
-        vector<int> ans;
-        ListNode *temp = head;
+        // vector<int> ans;
+        // ListNode *temp = head;
 
-        while(temp!=NULL){
-            ans.push_back(temp->val);
-            temp = temp->next;
+        // while(temp!=NULL){
+        //     ans.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+
+        // int i = ans.size()-1;
+        // temp = head;
+
+        // while(temp){
+        //     temp->val = ans[i];
+        //     i--;
+        //     temp = temp->next;
+        // }
+
+        // return head;
+
+
+          
+        ListNode *curr = head,*prev = NULL,*fut = NULL;
+
+        while(curr){
+            fut = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
         }
 
-        int i = ans.size()-1;
-        temp = head;
-
-        while(temp){
-            temp->val = ans[i];
-            i--;
-            temp = temp->next;
-        }
-
+        head = prev;
         return head;
     }
 };
